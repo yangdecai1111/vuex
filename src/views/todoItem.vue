651937febd72d1d08384475c1e64af7dd77c90cd
yangdecai1111 <461897887@qq.com>
2019-06-06 11:39:54 +0800

    vueX实例

---
+++ src/views/todoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>{{ todo.name }}-- <button @click="delTodo(todo)">删除</button></li>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  props: {
+    todo: Object,
+    index: Number
+  },
+  methods: {
+    ...mapActions(['delTodo'])
+  }
+}
+</script>
---
+++ src/views/todos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input type="text" v-model="inputVal"><button @click="fn1">添加</button>
+    <ul>
+      <todoItem v-for="(item, index) in todos" :key="index" :todo="item" :index="index"></todoItem>
+    </ul>
+  </div>
+</template>
+<script>
+import todoItem from './todoItem.vue'
+import { mapState, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['todos']),
+    inputVal: {
+      get () {
+        return this.$store.state.inputVal
+      },
+      set (value) {
+        this.$store.commit('chgInputVal', value)
+      }
+    }
+  },
+  components: {
+    todoItem
+  },
+  methods: {
+    ...mapActions(['fn1', 'initTodo'])
+  },
+  created () {
+    this.initTodo()
+  }
+}
+</script>
